26feaa6f77291229e2cd302e6298ea6cbe4a12eb
mcgoryflenniken134 <mcgoryflenniken134@gmail.com>
2025-03-08 05:17:51 +0000

    Add 3.cpp

---
+++ src/3.cpp
@@ -0,0 +1,10 @@
+
+int main() {
+  int age = 20;
+  if (age >= 18) {
+    std::cout << "You are old enough to vote!";
+  } else {
+    std::cout << "You are not old enough to vote.";
+  }
+  return 0;
+}
