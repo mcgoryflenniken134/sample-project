e0b43a107a366d5ff7a125e246248cf2470578a2
mcgoryflenniken134 <mcgoryflenniken134@gmail.com>
2025-03-12 05:49:30 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,13 @@
+int main() {
+    // Print a message to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    // Get input from the user
+    std::string name;
+    std::cin >> name;
+
+    // Display the input back to the user
+    std::cout << "Hello, " << name << "!" << std::endl;
+
+    return 0;
+}
