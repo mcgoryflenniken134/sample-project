5cfd36bceb73d8c8bbbcdf210a82015d66934f78
mcgoryflenniken134 <mcgoryflenniken134@gmail.com>
2025-03-14 08:52:11 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+  int num1 = 4;
+  int num2 = 5;
+  if(num1>num2) {
+    std::cout << "num1 is greater than num2";
+  } else if (num1<num2){
+    std::cout << "num1 is lesser than num2";
+  } else {
+    std::cout << "num1 and num2 are equal";
+  }
+}
