ed9c5e88993dc3590389f8d6dce57f98d014a57b
mcgoryflenniken134 <mcgoryflenniken134@gmail.com>
2025-05-28 00:04:12 +0000

    Add 42.cpp

---
+++ src/42.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    float z = 3.7f;
+
+    cout << "x = " << x << ", y = " << y << ", z = " << z << endl;
+
+    return 0;
+}
